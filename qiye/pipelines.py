326b748e80802f2932c88633a91111f531e9fdce
iamjacklee <jieke918@gmail.com>
2017-09-20 23:02:49 +0800

    second push

--- qiye/pipelines.py
+++ qiye/pipelines.py
@@ -4,8 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
+from scrapy.exceptions import DropItem
 
 class QiyePipeline(object):
+    def __init__(self):
+        self.file = open('papers.json','wb')
+
     def process_item(self, item, spider):
-        return item
+        if item['title']:
+            line = json.dumps(dict(item)) + '\n'
+            self.file.write(line)
+            return item
+        else:
+            raise DropItem('Missing title in %s' % item)
--- qiye/spiders/cnblogs.py
+++ qiye/spiders/cnblogs.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy import Selector
+from qiye.items import QiyeItem
 
 class CnblogsSpider(scrapy.Spider):
     name = 'cnblogs'
@@ -13,6 +14,18 @@ class CnblogsSpider(scrapy.Spider):
 
         for paper in papers:
             title = paper.xpath('.//*[@class="postTitle"]/a/text()').extract_first()
+            url = paper.xpath('.//div[@class="postTitle"]/a/@href').extract_first()
+            time = paper.xpath('.//div[@class="dayTitle"]/a/text()').extract_first()
+            content = paper.xpath('.//div[@class="c_b_p_desc"]/text()').extract_first()
+            item = QiyeItem(url=url,title=title,time=time,content=content)
+            yield item
 
-        print 'hello jacklee'
+            # print title,url,time
+            # print content
+        next_page = Selector(response).re(u'<a href="(\S*)">下一页</a>')
+        if next_page:
+            yield scrapy.Request(url=next_page[0],callback=self.parse)
+
+
+
             
